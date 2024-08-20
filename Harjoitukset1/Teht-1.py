a2abe0aca97d080e2f67b9c898a0f68f073589f9
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-08-20 11:39:38 +0300

    initial

---
+++ Harjoitukset1/Teht-1.py
@@ -0,0 +1,15 @@
+# Tulostetaaj juttuja, hästägit toimii kommentteina, muista tämä
+print("Hei Meeri Hurtta")
+print("""
+Moro
+tyyppä
+""")
+# \ on escape-markki
+
+# Syötteen lukeminen käyttäjältä
+command = "Tee jotain"
+print(command)
+#input()
+
+command = input ("Anna komento:")
+print("Rivinvaihto painettu, komento: "+command)
