bceb261352a1f07cdf7eb31cd7797ec3369c6be0
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-08-29 09:53:20 +0300

    Harjoitukset 5

---
+++ Harjoitukset 4/Teht-6.py
@@ -0,0 +1,26 @@
+#Piin likiarvon laskeminen
+#π≈4n/N, jossa n on yksikköympyrän sisälle sisälle tulevat pisteet, N on kaikki pisteet
+#epäyhtälöllä x^2+y^2<1 testataan, onko yksittäinen piste ympyrän sisällä
+
+import math
+import random
+iterator = 0
+N = input("Kuinka monta pistettä haluat testata?: ")
+N = int(N)
+#N = 10000000 #kaikki pisteet
+n = 0 #ympyrän sisälle osuvat pisteet
+while iterator < N:
+    iterator += 1
+    #arvotaan koordinaatit liukulukuina väliltä 1, -1, kaksi erillaista tapaa:
+    x = random.random() * 2 - 1
+    y = random.uniform (-1,1)
+    #print("Satunnainen piste: " + str(x) + ", " + str(y))
+    #print(x**2 + y**2 <1)
+    if x**2 + y**2 <1:
+        #print("Osui ympyrän sisälle")
+        n = n + 1
+print(f"{N} pisteestä {n} osui yksikköympyrän sisälle")
+result = 4*n / N
+print(f"Piin likiarvo on: {result}")
+print(f"Virhe: {result- math.pi}")
+
