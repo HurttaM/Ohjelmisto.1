8b747489e434f1368d38843c3a70735acd7a89af
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-09-09 13:20:05 +0300

    Harjoitukset 7

--- Harjoitukset 7/Teht-1.py
+++ Harjoitukset 7/Teht-1.py
@@ -1,4 +1,4 @@
-vuodenajat = ("talvi", "talvi", "talvi", "kevät", "kevät", "kevät", "kesä", "kesä", "syksy", "syksy", "syksy")
+vuodenajat = ("talvi", "talvi", "kevät", "kevät", "kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi")
 kknumero = int(input("Anna kuukauden numero (1-12): "))
 vuodenaika = vuodenajat[kknumero-1]
 print (f"{kknumero}. vuodenaika on {vuodenaika}.")
