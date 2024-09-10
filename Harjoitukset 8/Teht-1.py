c929b6302f5918eca0a1d9799ff6441ddbb0a47a
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-09-10 12:02:07 +0300

    Harjoitukset 8

--- Harjoitukset 8/Teht-1.py
+++ Harjoitukset 8/Teht-1.py
@@ -10,7 +10,9 @@ def lentoasema(koodi):
     sql = f"SELECT name, iso_country FROM airport where ident = '{koodi}'"
     cursor = connection.cursor()
     cursor.execute(sql)
-    print(cursor.fetchall())
+    result = cursor.fetchall()
+    for row in result:
+        print(f"Lentoaseman {row[0]} maakoodi on {row[1]}.")
 
 
 icao = input("Syötä lentoaseman ICAO-koodi: ").strip().upper()
