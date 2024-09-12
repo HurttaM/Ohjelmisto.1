3d89ae11e27d33ec32ecf12cddd2d4def26f0760
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-09-12 10:12:07 +0300

    Harjoitukset 8

--- Harjoitukset 8/Teht-3.py
+++ Harjoitukset 8/Teht-3.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from geopy import distance
 
 connection = mysql.connector.connect(host='localhost',
                                      database='flight_game',
@@ -6,3 +7,25 @@ connection = mysql.connector.connect(host='localhost',
                                      password='salasana',
                                      autocommit=True)
 
+def lentoasema(koodi):
+    koordinaatti = f"SELECT latitude_deg, longitude_deg from airport where ident = '{koodi}'"
+    cursor = connection.cursor()
+    cursor.execute(koordinaatti)
+    result = cursor.fetchone()
+    if result:
+        return result
+    else:
+        print(f"Lentokenttää ICAO-koodilla {koodi} ei löytynyt.")
+        return None
+
+
+icao1 = input("Syötä ensimmäisen lentoaseman ICAO-koodi: ").strip().upper()
+koordi1 = lentoasema(icao1)
+icao2 = input("Syötä toisen lentoaseman ICAO-koodi: ").strip().upper()
+koordi2 = lentoasema(icao2)
+
+if koordi1 and koordi2:
+    tulos = distance.distance(koordi1, koordi2).kilometers
+    print(f"Lentokenttien {icao1} ja {icao2} välinen etäisyys on {tulos:.2f} km.")
+else:
+    print("Etäisyyden laskeminen epäonnistui, koska koordinaatteja ei löytynyt.")
