4d4b9edec7c0b97b0433dd6fb2b70c01e6ccbc4d
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-10-28 09:29:24 +0200

    Harjoitukset 11

--- Harjoitukset 11/Teht-2.py
+++ Harjoitukset 11/Teht-2.py
@@ -22,7 +22,7 @@ class Auto:
         self.kuljettu_matka += kuljettu
 
 
-class Sähköauto(Auto):
+class Sahkoauto(Auto):
     def __init__(self, rekisteritunnus, huippunopeus, akkukapasiteetti):
         super().__init__(rekisteritunnus, huippunopeus)
         self.akkukapasiteetti = akkukapasiteetti
@@ -35,16 +35,16 @@ class Polttomoottoriauto(Auto):
 
 
 def paohjelma():
-    sähköauto = Sähköauto("ABC-15", 180, 52.5)
+    sahkoauto = Sahkoauto("ABC-15", 180, 52.5)
     polttomoottoriauto = Polttomoottoriauto("ACD-123", 165, 32.3)
 
-    sähköauto.kiihdyta(120)
+    sahkoauto.kiihdyta(120)
     polttomoottoriauto.kiihdyta(100)
 
-    sähköauto.kulje(3)
+    sahkoauto.kulje(3)
     polttomoottoriauto.kulje(3)
 
-    print(f"Sähköauton ({sähköauto.rekisteritunnus}) kuljettu matka: {sähköauto.kuljettu_matka} km")
+    print(f"Sähköauton ({sahkoauto.rekisteritunnus}) kuljettu matka: {sahkoauto.kuljettu_matka} km")
     print(f"Polttomoottoriauton ({polttomoottoriauto.rekisteritunnus}) kuljettu matka: {polttomoottoriauto.kuljettu_matka} km")
 
 paohjelma()
