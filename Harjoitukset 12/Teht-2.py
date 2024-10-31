84e822007e31d48a37e288a917359e0f23705e48
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-10-31 10:51:52 +0200

    Harjoitukset 12

--- Harjoitukset 12/Teht-2.py
+++ Harjoitukset 12/Teht-2.py
@@ -1,14 +1,15 @@
 import requests
 import json
 
-
 def hae_saa(paikka):
 
     url = f"http://api.openweathermap.org/data/2.5/weather?q={paikka}&units=metric&appid={'37922119c8b329ff65c9c70a23c7b3ae'}"
     vastaus = requests.get(url).json()
 
     if vastaus.get("cod") != 200:
-        print("Virhe. Yritä uudelleen.")
+        virhekoodi = vastaus.get("cod")
+        virheviesti = vastaus.get("message", "Tuntematon virhe")
+        print(f"Virhe ({virhekoodi}): {virheviesti}. Yritä uudelleen.")
         return
 
     saatila = vastaus["weather"][0]["description"]
@@ -19,6 +20,6 @@ def hae_saa(paikka):
     print(f"Lämpötila: {lampotila:.2f} °C")
 
 
-paikka = input("Anna paikan nimi: ")
+paikka = input("Anna kaupungin nimi: ")
 
 hae_saa(paikka)
